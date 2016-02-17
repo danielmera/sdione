581a853f0dff096ec3331b5e34f93d2d3e3f31b4
merolas <danielmeralopez@gmail.com>
2016-02-17 22:45:59 +0100

    Cambios en el main de la clase Sample

--- HelloWorld/src/Sample.java
+++ HelloWorld/src/Sample.java
@@ -3,6 +3,7 @@ import java.util.Date;
 public class Sample {
 
 	public static void main(String[] args) {
+		System.out.print("Esta es la fecha del día de hoy:\t");
 		System.out.println(new Date());
 	}
 

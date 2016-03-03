25afa2faf7ff2c9025e67c0a0a2f2cc4d6ab16af
merolas <danielmeralopez@gmail.com>
2016-03-03 17:30:50 +0100

    Pruebas

--- HelloWorld/src/Sample.java
+++ HelloWorld/src/Sample.java
@@ -1,10 +1,14 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.util.Calendar;
 import java.util.Date;
 
 public class Sample {
 
-	public static void main(String[] args) {
-		System.out.print("Esta es la fecha del día de hoy:\t");
-		System.out.println(new Date());
+	public static void main(String[] args) throws ParseException {
+		String inputDate = "27-06-2008 15:51:20";
+		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date myDate = format.parse(inputDate);
+		System.out.println(myDate);
 	}
-
 }

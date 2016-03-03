42971d098b8c7f39387e3929a4ddb32d95f4e5ec
merolas <danielmeralopez@gmail.com>
2016-03-03 17:34:30 +0100

    Nueva rama con cambios

--- HelloWorld/src/Sample.java
+++ HelloWorld/src/Sample.java
@@ -5,10 +5,18 @@ import java.util.Date;
 
 public class Sample {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
+		doSomething();
+	}
+
+	public static void doSomething() {
 		String inputDate = "27-06-2008 15:51:20";
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		Date myDate = format.parse(inputDate);
-		System.out.println(myDate);
+		try {
+			Date myDate = format.parse(inputDate);
+			System.out.println(myDate);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
